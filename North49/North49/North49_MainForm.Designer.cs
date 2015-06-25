0bc39e129476f40e571dc8fa42c812c0306ebc7a
Thanh Lai <thanhlai@outlook.com>
2015-06-24 21:27:41 -0700

    initial commit

---
+++ North49/North49/North49_MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace North49
+{
+    partial class North49_MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(North49_MainForm));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.introLabel = new System.Windows.Forms.Label();
+            this.alterTextBox = new System.Windows.Forms.TextBox();
+            this.removeButton = new System.Windows.Forms.PictureBox();
+            this.addButton = new System.Windows.Forms.PictureBox();
+            this.helpButton = new System.Windows.Forms.PictureBox();
+            this.browseButton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.removeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.browseButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 25);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(972, 270);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.Visible = false;
+            // 
+            // introLabel
+            // 
+            this.introLabel.AutoSize = true;
+            this.introLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introLabel.Location = new System.Drawing.Point(29, 45);
+            this.introLabel.Name = "introLabel";
+            this.introLabel.Size = new System.Drawing.Size(0, 16);
+            this.introLabel.TabIndex = 2;
+            // 
+            // alterTextBox
+            // 
+            this.alterTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alterTextBox.Location = new System.Drawing.Point(164, 351);
+            this.alterTextBox.Multiline = true;
+            this.alterTextBox.Name = "alterTextBox";
+            this.alterTextBox.Size = new System.Drawing.Size(588, 89);
+            this.alterTextBox.TabIndex = 6;
+            this.alterTextBox.TextChanged += new System.EventHandler(this.alterTextBox_TextChanged);
+            this.alterTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.alterTextBox_MouseDown);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.removeButton.Image = global::North49.Properties.Resources.remove;
+            this.removeButton.Location = new System.Drawing.Point(845, 371);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(48, 50);
+            this.removeButton.TabIndex = 8;
+            this.removeButton.TabStop = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addButton.Image = global::North49.Properties.Resources.add;
+            this.addButton.Location = new System.Drawing.Point(773, 372);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(50, 50);
+            this.addButton.TabIndex = 7;
+            this.addButton.TabStop = false;
+            // 
+            // helpButton
+            // 
+            this.helpButton.Cursor = System.Windows.Forms.Cursors.Help;
+            this.helpButton.Image = global::North49.Properties.Resources.help;
+            this.helpButton.Location = new System.Drawing.Point(922, 366);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(62, 59);
+            this.helpButton.TabIndex = 4;
+            this.helpButton.TabStop = false;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.browseButton.Image = global::North49.Properties.Resources.envelope;
+            this.browseButton.Location = new System.Drawing.Point(12, 351);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(126, 89);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.TabStop = false;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // North49_MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 452);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.alterTextBox);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.introLabel);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "North49_MainForm";
+            this.Text = "North49 - Thanh Lai";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.removeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.addButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.helpButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.browseButton)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label introLabel;
+        private System.Windows.Forms.PictureBox browseButton;
+        private System.Windows.Forms.PictureBox helpButton;
+        private System.Windows.Forms.TextBox alterTextBox;
+        private System.Windows.Forms.PictureBox addButton;
+        private System.Windows.Forms.PictureBox removeButton;
+    }
+}
+
---
+++ North49/North49/North49_MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * @author: Thanh Lai
+ * @project: North49 Technical Assessment
+ * @company: North49 Business Solutions 
+ * @supervisor: Michael VanKuipers
+ * @date: 24/06/2015
+ * @version: 1.0.0
+ */
+namespace North49
+{
+    public partial class North49_MainForm : Form
+    {
+        public North49_MainForm()
+        {
+            InitializeComponent();
+            SetUp();
+        }
+
+        // 1. Loads CSV and display into the grid view
+        private void LoadCSVToDataGridView(string filePath)
+        {
+            DataSet ds = new DataSet();
+            string tableName = "sampleTable";
+
+            StreamReader sr = new StreamReader(filePath);
+            //string csv = File.ReadAllText(filePath);
+            // Defines a new table to hold the data set
+            ds.Tables.Add(tableName);   //table's name
+            
+           
+
+            string[] rows = sr.ReadToEnd().Split("\n".ToCharArray());
+            int firstRow = 0; // just a flag so that it knows the first row's data will be the columns' attributes
+            foreach(string row in rows)
+            {
+                string[] records = row.Split(",".ToCharArray());
+                // Table's attributes: ID,Name,Address,City,State,Zip,Country / or first rows values as columns's attribute
+                if (firstRow == 0)
+                {
+                    for (int i = 0; i < records.Length; i++)
+                    {
+                        ds.Tables[tableName].Columns.Add(records[i]);
+                    }
+                }
+                else
+                {
+                    ds.Tables[tableName].Rows.Add(records);
+                }
+                firstRow++;
+            }
+            dataGridView.DataSource = ds.Tables[tableName].DefaultView;
+        }
+
+        private void SetUp()
+        {
+            // Intro and How-to texts
+            introLabel.Text = "Hello from Thanh, here is my little program which allows users to populate data from CSV files to the Data Grid View as well as to modify or alter the records.";
+            introLabel.Text += "\nGET STARTED BY: ";
+            introLabel.Text += "\n1. Clicking onto the envelope icon below to browse and select your CSV file";
+            introLabel.Text += "\n2. The program will automatically display the data in that CSV file to the Data Grid View";
+            introLabel.Text += "\n3. Hint: clicking on the help icon in the bottom left of the program for more helps or to see credits.";
+            addButton.Hide();
+            removeButton.Hide();
+            alterTextBox.Text = "Enter keyword here";
+            alterTextBox.Hide();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // csv error handling
+                if (ofd.FileName.EndsWith(".csv"))
+                {
+                    LoadCSVToDataGridView(ofd.FileName);
+                    introLabel.Hide();
+                    addButton.Show();
+                    removeButton.Show();
+                    alterTextBox.Show();
+                    dataGridView.Show();
+                }
+                else
+                {
+                    MessageBox.Show("The file you are trying to open is not supported by this program.", "Unsupported Extension", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void alterTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void alterTextBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            alterTextBox.Text = "";
+        }
+
+        // 2. Modifies records
+        // ------------------- ADD a new record
+        private void AddARecord()
+        {
+
+        }
+        // ------------------- REMOVE a record
+        private void RemoveARecord()
+        {
+
+        }
+    }
+}
